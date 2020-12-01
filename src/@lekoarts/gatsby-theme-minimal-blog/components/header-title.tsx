e0558b4af006dac1d5dbcec891669d00a0de5df1
kuzdogan <kaanuzdogan@hotmail.com>
2020-12-01 15:48:23 +0100

    Change title font and style, change favicon

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/header-title.tsx
@@ -0,0 +1,40 @@
+/** @jsx jsx */
+import useMinimalBlogConfig from "@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-minimal-blog-config";
+import useSiteMetadata from "@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-site-metadata";
+import replaceSlashes from "@lekoarts/gatsby-theme-minimal-blog/src/utils/replaceSlashes";
+import "fontsource-courier-prime";
+import { Link } from "gatsby";
+import { jsx, useColorMode } from "theme-ui";
+
+const HeaderTitle = () => {
+  const { siteTitle } = useSiteMetadata()
+  const { basePath } = useMinimalBlogConfig()
+  const [colorMode, setColorMode] = useColorMode()
+  const isDark = colorMode === `dark`
+
+  return (
+    <Link
+      to={replaceSlashes(`/${basePath}`)}
+      aria-label={`${siteTitle} - Back to home`}
+      sx={{ color: `heading`, textDecoration: `none` }}
+    >
+      <div sx={{ fontFamily: "Courier Prime", my: 0, fontWeight: `medium`, fontSize: [3, 4] }}>kaan{" "}
+
+        <div sx={{
+          display: 'inline-block',
+          backgroundColor: theme => isDark ? theme.colors.heading : theme.colors.heading,
+          color: theme => isDark ? theme.colors.background : theme.colors.background,
+          padding: "0 1px",
+          margin: "0 1px",
+          height: ["1.8rem", "2.2rem"]
+        }}
+        >
+          uz
+        </div>
+
+      dogan</div>
+    </Link>
+  )
+}
+
+export default HeaderTitle
