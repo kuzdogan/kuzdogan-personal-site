3801b013e0656c92496e89aaf636a04f8a91c758
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2025-05-25 21:36:01 +0200

    Adjust header title, theme switch

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import 'css/tailwind.css'
 import 'pliny/search/algolia.css'
 import 'remark-github-blockquote-alert/alert.css'
 
-import { Space_Grotesk } from 'next/font/google'
+import { Space_Grotesk, Courier_Prime } from 'next/font/google'
 import { Analytics, AnalyticsConfig } from 'pliny/analytics'
 import { SearchProvider, SearchConfig } from 'pliny/search'
 import Header from '@/components/Header'
@@ -16,6 +16,13 @@ const space_grotesk = Space_Grotesk({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-space-grotesk',
+})
+
+const courier_prime = Courier_Prime({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-courier-prime',
+  weight: ['400', '700'],
 })
 
 export const metadata: Metadata = {
@@ -64,7 +71,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html
       lang={siteMetadata.language}
-      className={`${space_grotesk.variable} scroll-smooth`}
+      className={`${space_grotesk.variable} ${courier_prime.variable} scroll-smooth`}
       suppressHydrationWarning
     >
       <link
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,31 +1,28 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
 
 const Header = () => {
-  let headerClass = 'flex items-center w-full bg-white dark:bg-gray-950 justify-between py-10'
+  let headerClass = 'flex items-center w-full bg-white dark:bg-gray-900 justify-between py-10'
   if (siteMetadata.stickyNav) {
     headerClass += ' sticky top-0 z-50'
   }
 
   return (
     <header className={headerClass}>
-      <Link href="/" aria-label={siteMetadata.headerTitle}>
-        <div className="flex items-center justify-between">
-          <div className="mr-3">
-            <Logo />
-          </div>
-          {typeof siteMetadata.headerTitle === 'string' ? (
-            <div className="hidden h-6 text-2xl font-semibold sm:block">
-              {siteMetadata.headerTitle}
-            </div>
-          ) : (
-            siteMetadata.headerTitle
-          )}
+      <Link
+        href="/"
+        className="font-courier-prime cursor-pointer text-gray-900 no-underline dark:text-gray-100"
+      >
+        <div className="text-2xl font-medium md:text-3xl">
+          kaan{' '}
+          <span className="mx-[1px] inline-block h-[1.8rem] bg-gray-900 px-[1px] pt-[0.25rem] text-white md:h-[2.2rem] md:pt-[0.4rem] dark:bg-gray-100 dark:text-gray-900">
+            uz
+          </span>
+          dogan
         </div>
       </Link>
       <div className="flex items-center space-x-4 leading-5 sm:-mr-6 sm:space-x-6">
