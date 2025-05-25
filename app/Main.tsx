633cf3843e73a46698515c4cde1b0b6d43a72c52
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2025-05-25 22:18:31 +0200

    Update layout

--- app/Main.tsx
+++ app/Main.tsx
@@ -4,67 +4,62 @@ import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 import NewsletterForm from 'pliny/ui/NewsletterForm'
 
-const MAX_DISPLAY = 5
+const MAX_DISPLAY = 4
 
 export default function Home({ posts }) {
   return (
     <>
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
-            Latest
+      <div className="space-y-2">
+        <div className="space-y-2 pt-6 pb-8">
+          <h1 className="text-3xl font-extrabold tracking-tight text-gray-900 md:text-6xl dark:text-gray-100">
+            Hi!
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            {siteMetadata.description}
-          </p>
+          <div className="prose dark:prose-invert">
+            <p className="md:text-lg">I'm Kaan. This is my place on the internet 🌎.</p>
+            <p className="md:text-lg">
+              I'm a curious computer science grad and software developer from Turkey 🇹🇷, currently
+              based in Berlin 🇩🇪.
+            </p>
+            <p className="md:text-lg">
+              I am working on <Link href="https://sourcify.dev">Sourcify</Link> at the Ethereum
+              Foundation.
+            </p>
+            <p className="md:text-lg">
+              I write my thoughts, experiences, and some technical stuff here.
+            </p>
+            <p className="md:text-lg">
+              <Link href="/about">More about me</Link>.
+            </p>
+          </div>
         </div>
-        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="flex flex-row items-center justify-between">
+          <h2 className="text-2xl font-bold text-gray-900 md:text-3xl dark:border-gray-700 dark:text-gray-100">
+            Latest Posts
+          </h2>
+          <Link
+            href="/blog"
+            className="hover:text-primary-600 dark:hover:text-primary-400 text-sm md:text-base"
+          >
+            Read All Posts &rarr;
+          </Link>
+        </div>
+        <ul className="">
           {!posts.length && 'No posts found.'}
           {posts.slice(0, MAX_DISPLAY).map((post) => {
-            const { slug, date, title, summary, tags } = post
+            const { slug, date, title } = post
             return (
-              <li key={slug} className="py-12">
-                <article>
-                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                    <dl>
-                      <dt className="sr-only">Published on</dt>
-                      <dd className="text-base leading-6 font-medium text-gray-500 dark:text-gray-400">
-                        <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
-                      </dd>
-                    </dl>
-                    <div className="space-y-5 xl:col-span-3">
-                      <div className="space-y-6">
-                        <div>
-                          <h2 className="text-2xl leading-8 font-bold tracking-tight">
-                            <Link
-                              href={`/blog/${slug}`}
-                              className="text-gray-900 dark:text-gray-100"
-                            >
-                              {title}
-                            </Link>
-                          </h2>
-                          <div className="flex flex-wrap">
-                            {tags.map((tag) => (
-                              <Tag key={tag} text={tag} />
-                            ))}
-                          </div>
-                        </div>
-                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                          {summary}
-                        </div>
-                      </div>
-                      <div className="text-base leading-6 font-medium">
-                        <Link
-                          href={`/blog/${slug}`}
-                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                          aria-label={`Read more: "${title}"`}
-                        >
-                          Read more &rarr;
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-                </article>
+              <li key={slug} className="py-1">
+                <h2 className="text-lg tracking-tight">
+                  <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
+                    {title}
+                  </Link>{' '}
+                  <dl className="inline-flex items-center">
+                    <dt className="sr-only">Published on</dt>
+                    <dd className="text-xs leading-6 font-medium text-gray-400 dark:text-gray-500">
+                      <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
+                    </dd>
+                  </dl>
+                </h2>
               </li>
             )
           })}
@@ -81,11 +76,11 @@ export default function Home({ posts }) {
           </Link>
         </div>
       )}
-      {siteMetadata.newsletter?.provider && (
+      {/* {siteMetadata.newsletter?.provider && (
         <div className="flex items-center justify-center pt-4">
           <NewsletterForm />
         </div>
-      )}
+      )} */}
     </>
   )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -6,17 +6,14 @@ import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
 
 const Header = () => {
-  let headerClass = 'flex items-center w-full bg-white dark:bg-gray-900 justify-between py-10'
+  let headerClass = 'flex items-center w-full justify-between py-6 md:py-8'
   if (siteMetadata.stickyNav) {
     headerClass += ' sticky top-0 z-50'
   }
 
   return (
     <header className={headerClass}>
-      <Link
-        href="/"
-        className="font-courier-prime cursor-pointer text-gray-900 no-underline dark:text-gray-100"
-      >
+      <Link href="/" className="font-courier-prime cursor-pointer">
         <div className="text-2xl font-medium md:text-3xl">
           kaan{' '}
           <span className="mx-[1px] inline-block h-[1.8rem] bg-gray-900 px-[1px] pt-[0.25rem] text-white md:h-[2.2rem] md:pt-[0.4rem] dark:bg-gray-100 dark:text-gray-900">
@@ -25,7 +22,7 @@ const Header = () => {
           dogan
         </div>
       </Link>
-      <div className="flex items-center space-x-4 leading-5 sm:-mr-6 sm:space-x-6">
+      <div className="flex items-center space-x-4 leading-5 sm:space-x-5">
         <div className="no-scrollbar hidden max-w-40 items-center gap-x-4 overflow-x-auto sm:flex md:max-w-72 lg:max-w-96">
           {headerNavLinks
             .filter((link) => link.href !== '/')
