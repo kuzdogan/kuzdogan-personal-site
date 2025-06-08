3372c2754e17d2ad8817ce69d2f61766a9a7cd45
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2025-06-08 22:21:43 +0200

    Fonts, sizes, colors for legibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import 'css/tailwind.css'
 import 'pliny/search/algolia.css'
 import 'remark-github-blockquote-alert/alert.css'
 
-import { Raleway, Courier_Prime } from 'next/font/google'
+import { Courier_Prime, Atkinson_Hyperlegible } from 'next/font/google'
 import { Analytics, AnalyticsConfig } from 'pliny/analytics'
 import { SearchProvider, SearchConfig } from 'pliny/search'
 import Header from '@/components/Header'
@@ -12,10 +12,11 @@ import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
 
-const raleway = Raleway({
+const raleway = Atkinson_Hyperlegible({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-raleway',
+  weight: ['400', '700'],
 })
 
 const courier_prime = Courier_Prime({
